a8bce0e8731c1d2b456be85274f3d59c31526b6b
Helge Gehring <helge.gehring@uni-muenster.de>
2019-09-25 02:01:13 +0200

    added test for find_line_intersection

--- gdshelpers/tests/test_helpers.py
+++ gdshelpers/tests/test_helpers.py
@@ -1,6 +1,8 @@
 import unittest
+import numpy as np
+import numpy.testing as npt
 
-from gdshelpers.helpers import int_to_alphabet, id_to_alphanumeric
+from gdshelpers.helpers import int_to_alphabet, id_to_alphanumeric, find_line_intersection
 
 
 class HelpersTestCase(unittest.TestCase):
@@ -12,3 +14,8 @@ class HelpersTestCase(unittest.TestCase):
 
     def test_id_to_alphanumeric(self):
         self.assertEqual(id_to_alphanumeric(4, 26 + 25), 'AZ4')
+
+    def test_find_line_intersection(self):
+        test_intersection = find_line_intersection(np.array((2, 0)), np.pi / 2, np.array((0, 1)), 0)
+        npt.assert_almost_equal(test_intersection[0], (2, 1))
+        npt.assert_almost_equal(test_intersection[1], (1, 2))
