d79c08d1ae6e1cd8d553cd3af8a3e3c5cedf7036
ThomasGrottke <Thomas.Grottke@wwu.de>
2019-11-08 16:02:14 +0100

    Comments added, example added, variables reorganized.

--- gdshelpers/parts/mode_converter.py
+++ gdshelpers/parts/mode_converter.py
@@ -4,45 +4,73 @@ from gdshelpers.parts.waveguide import Waveguide
 from gdshelpers.parts import Port
 
 
-class StripToSlotModeConverter(object):
-    def __init__(self, origin, angle, width, taper_length, final_width):
+class StripToSlotModeConverter:
+    """
+    Generates a strip to slot mode converter as presented by Palmer et. al https://ieeexplore.ieee.org/document/6412699.
+    If the input port width is a scalar und the final width is an array, a strip to slot mode converter is generated.
+    On the other hand, if the input port width is an array and the final width is a scaler, a slot to strip mode
+    converter is generated.
+
+    """
+
+    def __init__(self, origin, angle, width, taper_length, final_width, front_taper_length, front_taper_width):
         self._taper_length = taper_length
         self._in_port = Port(origin, angle, width)
         self._final_width = final_width
-        self._length_taper_front = 1
-        self._width_taper_front = 0.2
-
+        self._front_taper_length = front_taper_length
+        self._front_taper_width = front_taper_width
         self._waveguide = None
 
     @classmethod
-    def make_at_port(cls, port, taper_length, final_width):
-        return cls(port.origin, port.angle, port.width, taper_length, final_width)
+    def make_at_port(cls, port, taper_length, final_width, front_taper_length=2, front_taper_width=0.2):
+        return cls(port.origin, port.angle, port.width, taper_length, final_width, front_taper_length,
+                   front_taper_width)
 
     @property
     def in_port(self):
+        """
+        Returns the input port of the mode converter
+
+        :return: port
+        """
         return self._in_port.copy()
 
     @property
     def out_port(self):
-        port = self._in_port.longitudinal_offset(self._taper_length + self._length_taper_front)
+        """
+        Returns the output port of the mode converter
+
+        :return: port
+        """
+        port = self._in_port.longitudinal_offset(self._taper_length + self._front_taper_length)
         port.width = self._final_width
         return port
 
     def get_shapely_object(self):
+        """
+        Generates the mode converter. If the input port width is a scalar und the final width is an array, a strip to
+        slot mode converter is generated. On the other hand, if the input port width is an array and the final width is
+        a scaler, a slot to strip mode converter is generated.
+
+        :return: shapely object
+        """
         if self._waveguide:
             return self._waveguide.get_shapely_object()
 
+
         if np.array(self._in_port.width).size == 1:
+            # strip to slot mode converter
             strip_width = self.in_port.width
             slot_width = self._final_width
         else:
+            # slot to strip mode converter
             slot_width = self.in_port.width
             strip_width = self._final_width
 
         def pre_taper_width(t):
             return [
-                slot_width[0] * t + self._width_taper_front * (1 - t),
-                slot_width[1] + (slot_width[0] - self._width_taper_front) * (1 - t),
+                slot_width[0] * t + self._front_taper_width * (1 - t),
+                slot_width[1] + (slot_width[0] - self._front_taper_width) * (1 - t),
                 strip_width,
                 slot_width[0] + slot_width[1]
             ]
@@ -56,12 +84,42 @@ class StripToSlotModeConverter(object):
         self._waveguide = Waveguide.make_at_port(self._in_port)
 
         if np.array(self._in_port.width).size == 1:
-            self._waveguide.add_parameterized_path(lambda t: [t * self._length_taper_front, 0], width=pre_taper_width)
+            # strip to slot mode converter
+            self._waveguide.add_parameterized_path(lambda t: [t * self._front_taper_length, 0], width=pre_taper_width)
             self._waveguide.add_parameterized_path(lambda t: [t * self._taper_length, 0], width=taper_width)
         else:
+            # slot to strip mode converter
             self._waveguide.add_parameterized_path(lambda t: [t * self._taper_length, 0],
                                                    width=lambda t: taper_width(1 - t))
-            self._waveguide.add_parameterized_path(lambda t: [t * self._length_taper_front, 0],
+            self._waveguide.add_parameterized_path(lambda t: [t * self._front_taper_length, 0],
                                                    width=lambda t: pre_taper_width(1 - t))
 
         return self._waveguide.get_shapely_object()
+
+
+def _example():
+    from gdshelpers.geometry.chip import Cell
+    from gdshelpers.parts.port import Port
+    from gdshelpers.parts.waveguide import Waveguide
+    from gdshelpers.parts.mode_converter import StripToSlotModeConverter
+
+    wg_1 = Waveguide.make_at_port(Port(origin=(0, 0), angle=0, width=1.2))  # array as width -> slot waveguide
+    wg_1.add_straight_segment(5)
+
+    mc_1 = StripToSlotModeConverter.make_at_port(wg_1.current_port, 5, [0.4, 0.2, 0.4])
+
+    wg_2 = Waveguide.make_at_port(mc_1.out_port)
+    wg_2.add_bend(angle=np.pi, radius=5)
+
+    mc_2 = StripToSlotModeConverter.make_at_port(wg_2.current_port, 5, 1)
+
+    wg_3 = Waveguide.make_at_port(mc_2.out_port)
+    wg_3.add_straight_segment(5)
+
+    cell = Cell('CELL')
+    cell.add_to_layer(1, wg_1, mc_1, wg_2, mc_2, wg_3)
+    cell.show()
+
+
+if __name__ == '__main__':
+    _example()
