ed2b89a6a4ff967f244b682640b77d84f2561b16
Helge Gehring <helge.gehring@uni-muenster.de>
2020-09-16 14:54:41 +0200

    added warning for python version 3.5 and below, stopped testing for python 3.5 (as 3.5 reached its end of life)

--- gdshelpers/__init__.py
+++ gdshelpers/__init__.py
@@ -1,5 +1,13 @@
 # noinspection PyProtectedMember
 from gdshelpers._global_configuration import _Configuration
+import sys
+
+if sys.version_info < (3, 6, 0):
+    import warnings
+
+    warnings.warn(
+        'The installed Python version reached its end-of-life. Please upgrade to a newer Python version for receiving '
+        'further gdshelpers updates.', Warning)
 
 configuration = _Configuration()
 
